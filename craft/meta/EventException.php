b288dfd711cbe01128a0cadccc7cfdea557eabc5
Aymeric Assier <aymeric.assier@infomaniak.ch>
2013-12-10 10:53:49 +0100

    Add license header

--- craft/meta/EventException.php
+++ craft/meta/EventException.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * This file is part of the Craft package.
+ *
+ * Copyright Aymeric Assier <aymeric.assier@gmail.com>
+ *
+ * For the full copyright and license information, please view the Licence.txt
+ * file that was distributed with this source code.
+ */
 namespace craft\meta;
 
 class EventException extends \Exception
