e7dcaf826bc509d5b72e2a73de4ca6c8793e0337
Aymeric Assier <aymeric.assier@infomaniak.ch>
2014-02-18 11:33:33 +0100

    Move Event to Reflect package, fix App\Bubble

--- Vendor/Craft/Kernel/App/Bubble.php
+++ Vendor/Craft/Kernel/App/Bubble.php
@@ -7,11 +7,11 @@
  * For the full copyright and license information, please view the Licence.txt
  * file that was distributed with this source code.
  */
-namespace Craft\App\Preset;
+namespace Craft\Kernel\App;
 
 use Craft\Kernel\App;
 use Craft\Reflect\Action;
-use Craft\View\Viewable;
+use Craft\View\Engine;
 
 class Bubble extends App
 {
@@ -31,13 +31,6 @@ class Bubble extends App
      */
     public function __construct($dir, $ext = 'php', array $helpers = [])
     {
-        // set config
-        $config = [
-            'template.dir'  => $dir,
-            'template.ext'  => $ext,
-            'template.helpers' => $helpers + $this->config['template.helpers']
-        ];
-
         // routing
         $routes = [
             '/'     => [$this, 'page'],
@@ -45,10 +38,10 @@ class Bubble extends App
         ];
 
         // init dispatcher
-        parent::__construct($routes, $config);
+        parent::__construct($routes);
 
         // intercept template
-        $this->on('dispatcher.render', function(Viewable $view, Action $action){
+        $this->on('dispatcher.render', function(Engine $engine, Action $action){
             $action->metadata['render'] = $action->data['template'];
         });
 
---
+++ Vendor/Craft/Reflect/Event.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * This file is part of the Craft package.
+ *
+ * Copyright Aymeric Assier <aymeric.assier@gmail.com>
+ *
+ * For the full copyright and license information, please view the Licence.txt
+ * file that was distributed with this source code.
+ */
+namespace Craft\Reflect;
+
+trait Event
+{
+
+    /** @var array */
+    protected $listeners = [];
+
+    /** @var array */
+    protected $locked = [];
+
+
+    /**
+     * Attach callback
+     * @param string $event
+     * @param callable $callable
+     * @param string $key
+     * @throws \LogicException
+     * @return $this
+     */
+    public function on($event, callable $callable, $key = null)
+    {
+        // locked, cannot attach
+        if(isset($this->locked[$event]) and !$this->locked[$event] == $key) {
+            throw new \LogicException('Event "' . $event . '" is locked.');
+        }
+
+        $this->listeners[$event][] = $callable;
+        return $this;
+    }
+
+
+    /**
+     * Lock event attaching
+     * @param $event
+     * @param $key
+     * @return $this
+     */
+    protected function lock($event, $key = null)
+    {
+        $this->locked[$event] = $key ?: uniqid();
+        return $this;
+    }
+
+
+    /**
+     * Unlock attaching
+     * @param $event
+     * @return $this
+     */
+    protected function unlock($event)
+    {
+        unset($this->locked[$event]);
+        return $this;
+    }
+
+
+    /**
+     * Fire event
+     * @param string $event
+     * @param array $params
+     * @return int
+     */
+    protected function fire($event, array $params = [])
+    {
+        // fire * event
+        if($event != '*') {
+            $params['_initial'] = $event;
+            $this->fire('*', $params);
+        }
+
+        // no listeners
+        if(!isset($this->listeners[$event])) {
+            return false;
+        }
+
+        // trigger all listeners
+        foreach($this->listeners[$event] as $callable){
+            call_user_func_array($callable, $params);
+        }
+
+        // total triggered
+        return count($this->listeners[$event]);
+    }
+
+}
